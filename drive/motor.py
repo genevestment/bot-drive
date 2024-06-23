09a23b0a71b9e024cea6f71fd398e2c9b862479c
Tony Yang <cloud.genevestment@gmail.com>
2024-06-22 22:20:39 -0700

    Refactor the common drive mechanism logic and group the same wheel rotation and orientation together

--- drive/motor.py
+++ drive/motor.py
@@ -16,130 +16,95 @@ class Motors:
             return -1.0
         return x
 
-    def left_front_motor(self, channel_input):
-        ch1_left_front = 0.0
-        ch2_left_front = 0.0
+    # up right wheel orientation defines the mecanum wheel who's rolling direction is towards
+    # the up and right direction. This applies to the left front wheel and right back wheel.
+    # They have the same orientation and rotation.
+    def _up_right_wheel_orientation_drive(self, channel_input) -> float:
+        ch1_speed_multiplier = 0.0
+        ch2_speed_multiplier = 0.0
+        speed_multiplier = 0.0
         if channel_input.channels[1] > channel_input.channels_position[1].neutral_high:
-            ch1_left_front = self.bounded_motor_speed(
+            ch1_speed_multiplier = self.bounded_motor_speed(
                 (channel_input.channels[1] - channel_input.channels_position[1].neutral_high) /
                 (channel_input.channels_position[1].highest -
                  channel_input.channels_position[1].neutral_high))
         elif channel_input.channels[1] < channel_input.channels_position[1].neutral_low:
-            ch1_left_front = self.bounded_motor_speed(
+            ch1_speed_multiplier = self.bounded_motor_speed(
                 (channel_input.channels[1] - channel_input.channels_position[1].neutral_low) /
                 (channel_input.channels_position[1].neutral_low -
                  channel_input.channels_position[1].lowest))
 
         if channel_input.channels[2] > channel_input.channels_position[2].neutral_high:
-            ch2_left_front = self.bounded_motor_speed(
+            ch2_speed_multiplier = self.bounded_motor_speed(
                 (channel_input.channels[2] - channel_input.channels_position[2].neutral_high) /
                 (channel_input.channels_position[2].highest -
                  channel_input.channels_position[2].neutral_high))
         elif channel_input.channels[2] < channel_input.channels_position[2].neutral_low:
-            ch2_left_front = self.bounded_motor_speed(
+            ch2_speed_multiplier = self.bounded_motor_speed(
                 (channel_input.channels[2] - channel_input.channels_position[2].neutral_low) /
                 (channel_input.channels_position[2].neutral_low -
                  channel_input.channels_position[2].lowest))
 
-        if ch1_left_front <= 0 and ch2_left_front <= 0:
-            self.left_front = min(ch1_left_front, ch2_left_front)
-        elif ch1_left_front >= 0 and ch2_left_front >= 0:
-            self.left_front = max(ch1_left_front, ch2_left_front)
-        elif (ch1_left_front <= 0 and ch2_left_front >= 0) or (ch1_left_front >= 0 and
-                                                               ch2_left_front <= 0):
-            self.left_front = ch2_left_front + ch1_left_front
+        if ch1_speed_multiplier <= 0 and ch2_speed_multiplier <= 0:
+            speed_multiplier = min(ch1_speed_multiplier, ch2_speed_multiplier)
+        elif ch1_speed_multiplier >= 0 and ch2_speed_multiplier >= 0:
+            speed_multiplier = max(ch1_speed_multiplier, ch2_speed_multiplier)
+        elif (ch1_speed_multiplier <= 0 and
+              ch2_speed_multiplier >= 0) or (ch1_speed_multiplier >= 0 and
+                                             ch2_speed_multiplier <= 0):
+            speed_multiplier = ch1_speed_multiplier + ch2_speed_multiplier
+        return speed_multiplier
+
+    # up left wheel orientation defines the mecanum wheel who's rolling direction is towards
+    # the up and left direction. This applies to the right front wheel and left back wheel.
+    # They have the same orientation and rotation.
+    def _up_left_wheel_orientation_drive(self, channel_input) -> float:
+        ch1_speed_multiplier = 0.0
+        ch2_speed_multiplier = 0.0
+        speed_multiplier = 0.0
+        if channel_input.channels[1] > channel_input.channels_position[1].neutral_high:
+            ch1_speed_multiplier = self.bounded_motor_speed(
+                (channel_input.channels_position[1].neutral_high - channel_input.channels[1]) /
+                (channel_input.channels_position[1].highest -
+                 channel_input.channels_position[1].neutral_high))
+        elif channel_input.channels[1] < channel_input.channels_position[1].neutral_low:
+            ch1_speed_multiplier = self.bounded_motor_speed(
+                (channel_input.channels_position[1].neutral_low - channel_input.channels[1]) /
+                (channel_input.channels_position[1].neutral_low -
+                 channel_input.channels_position[1].lowest))
+
+        if channel_input.channels[2] > channel_input.channels_position[2].neutral_high:
+            ch2_speed_multiplier = self.bounded_motor_speed(
+                (channel_input.channels[2] - channel_input.channels_position[2].neutral_high) /
+                (channel_input.channels_position[2].highest -
+                 channel_input.channels_position[2].neutral_high))
+        elif channel_input.channels[2] < channel_input.channels_position[2].neutral_low:
+            ch2_speed_multiplier = self.bounded_motor_speed(
+                (channel_input.channels[2] - channel_input.channels_position[2].neutral_low) /
+                (channel_input.channels_position[2].neutral_low -
+                 channel_input.channels_position[2].lowest))
+
+        if ch1_speed_multiplier <= 0 and ch2_speed_multiplier <= 0:
+            speed_multiplier = min(ch1_speed_multiplier, ch2_speed_multiplier)
+        elif ch1_speed_multiplier >= 0 and ch2_speed_multiplier >= 0:
+            speed_multiplier = max(ch1_speed_multiplier, ch2_speed_multiplier)
+        elif (ch1_speed_multiplier <= 0 and
+              ch2_speed_multiplier >= 0) or (ch1_speed_multiplier >= 0 and
+                                             ch2_speed_multiplier <= 0):
+            speed_multiplier = ch1_speed_multiplier + ch2_speed_multiplier
+        return speed_multiplier
+
+    def left_front_motor(self, channel_input):
+        self.left_front = self._up_right_wheel_orientation_drive(channel_input=channel_input)
 
     def right_front_motor(self, channel_input):
-        ch1_right_front = 0.0
-        ch2_right_front = 0.0
-        print(f'channels = {channel_input.channels}')
-        print(f'channel positions = {channel_input.channels_position}')
-        if channel_input.channels[1] > channel_input.channels_position[1].neutral_high:
-            ch1_right_front = self.bounded_motor_speed(
-                (channel_input.channels_position[1].neutral_high - channel_input.channels[1]) /
-                (channel_input.channels_position[1].highest -
-                 channel_input.channels_position[1].neutral_high))
-        elif channel_input.channels[1] < channel_input.channels_position[1].neutral_low:
-            ch1_right_front = self.bounded_motor_speed(
-                (channel_input.channels_position[1].neutral_low - channel_input.channels[1]) /
-                (channel_input.channels_position[1].neutral_low -
-                 channel_input.channels_position[1].lowest))
-
-        if channel_input.channels[2] > channel_input.channels_position[2].neutral_high:
-            ch2_right_front = self.bounded_motor_speed(
-                (channel_input.channels[2] - channel_input.channels_position[2].neutral_high) /
-                (channel_input.channels_position[2].highest -
-                 channel_input.channels_position[2].neutral_high))
-        elif channel_input.channels[2] < channel_input.channels_position[2].neutral_low:
-            ch2_right_front = self.bounded_motor_speed(
-                (channel_input.channels[2] - channel_input.channels_position[2].neutral_low) /
-                (channel_input.channels_position[2].neutral_low -
-                 channel_input.channels_position[2].lowest))
-
-        print(f'ch1 right front = {ch1_right_front}, ch2 right front = {ch2_right_front}')
-        if ch1_right_front <= 0 and ch2_right_front <= 0:
-            self.right_front = min(ch1_right_front, ch2_right_front)
-        elif ch1_right_front >= 0 and ch2_right_front >= 0:
-            self.right_front = max(ch1_right_front, ch2_right_front)
-        elif (ch1_right_front <= 0 and ch2_right_front >= 0) or (ch1_right_front >= 0 and
-                                                                 ch2_right_front <= 0):
-            self.right_front = ch2_right_front + ch1_right_front
+        self.right_front = self._up_left_wheel_orientation_drive(channel_input=channel_input)
 
     def left_back_motor(self, channel_input):
-        ch1_left_back = 0.0
-        ch2_left_back = 0.0
-
-        if channel_input.channels[1] > channel_input.channels_position[1].neutral_high:
-            ch1_left_back = self.bounded_motor_speed(
-                (channel_input.channels_position[1].neutral_high - channel_input.channels[1]) /
-                (channel_input.channels_position[1].highest -
-                 channel_input.channels_position[1].neutral_high))
-        elif channel_input.channels[1] < channel_input.channels_position[1].neutral_low:
-            ch1_left_back = self.bounded_motor_speed(
-                (channel_input.channels_position[1].neutral_low - channel_input.channels[1]) /
-                (channel_input.channels_position[1].neutral_low -
-                 channel_input.channels_position[1].lowest))
-
-        if channel_input.channels[2] > channel_input.channels_position[2].neutral_high:
-            ch2_left_back = self.bounded_motor_speed(
-                (channel_input.channels[2] - channel_input.channels_position[2].neutral_high) /
-                (channel_input.channels_position[2].highest -
-                 channel_input.channels_position[2].neutral_high))
-        elif channel_input.channels[2] < channel_input.channels_position[2].neutral_low:
-            ch2_left_back = self.bounded_motor_speed(
-                (channel_input.channels[2] - channel_input.channels_position[2].neutral_low) /
-                (channel_input.channels_position[2].neutral_low -
-                 channel_input.channels_position[2].lowest))
-
-        self.left_back = max(ch1_left_back, ch2_left_back)
+        self.left_back = self._up_left_wheel_orientation_drive(channel_input=channel_input)
 
     def right_back_motor(self, channel_input):
-        ch1_right_back = 0.0
-        ch2_right_back = 0.0
-
-        if channel_input.channels[1] > channel_input.channels_position[1].neutral_high:
-            ch1_right_back = self.bounded_motor_speed(
-                (channel_input.channels[1] - channel_input.channels_position[1].neutral_high) /
-                (channel_input.channels_position[1].highest -
-                 channel_input.channels_position[1].neutral_high))
-        elif channel_input.channels[1] < channel_input.channels_position[1].neutral_low:
-            ch1_right_back = self.bounded_motor_speed(
-                (channel_input.channels[1] - channel_input.channels_position[1].neutral_low) /
-                (channel_input.channels_position[1].neutral_low -
-                 channel_input.channels_position[1].lowest))
-
-        if channel_input.channels[2] > channel_input.channels_position[2].neutral_high:
-            ch2_right_back = self.bounded_motor_speed(
-                (channel_input.channels[2] - channel_input.channels_position[2].neutral_high) /
-                (channel_input.channels_position[2].highest -
-                 channel_input.channels_position[2].neutral_high))
-        elif channel_input.channels[2] < channel_input.channels_position[2].neutral_low:
-            ch2_right_back = self.bounded_motor_speed(
-                (channel_input.channels[2] - channel_input.channels_position[2].neutral_low) /
-                (channel_input.channels_position[2].neutral_low -
-                 channel_input.channels_position[2].lowest))
-
-        self.left_front = max(ch1_right_back, ch2_right_back)
+        self.right_back = self._up_right_wheel_orientation_drive(channel_input=channel_input)
 
     def manual_drive(self, channel_input):
         print(f'Manual drive with input: {channel_input}')
